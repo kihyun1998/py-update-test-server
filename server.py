6b40c36ef51dab6d22f284ae8e9fa625b0bb217e
박기현 <kihyun1998@naver.com>
2024-10-07 13:25:18 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,27 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
+import json
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
+
+    def do_GET(self):
+        if self.path == '/update/updatefilename':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            update_folder = 'update'
+            files = os.listdir(update_folder)
+            zip_files = [f for f in files if f.endswith('.zip')]
+            if zip_files:
+                latest_file = max(zip_files, key=lambda f: os.path.getmtime(os.path.join(update_folder, f)))
+                self.wfile.write(json.dumps({"filename": latest_file}).encode())
+            else:
+                self.wfile.write(json.dumps({"error": "No update file found"}).encode())
+        else:
+            super().do_GET()
 
 def run(server_class=HTTPServer, handler_class=CORSRequestHandler, port=8000):
     server_address = ('', port)
